b4552f29c944756029811a561bab14b50a422c4d
Felix Lima <32372236+ofelixlima@users.noreply.github.com>
2026-01-21 00:24:24 -0300

    Atualizando todos os arquivos que modifiquei
    VS Code ainda dando problema para commitar...

--- automacao-microsoft-rewards.py
+++ automacao-microsoft-rewards.py
@@ -1,6 +1,6 @@
 from time import sleep
 from random import randint
-from funcoes import mouseClique, tamanhoTela, posicaoMouse, moverMouse, apertarBotao, atalhoTeclado, cliqueDuplo, escrever, mouseCliqueAqui, moverPara, segurarBotao, soltarBotao
+from funcoes import mouseClique, apertarBotao, atalhoTeclado, escrever, moverPara
 
 def abrirEdge():
     apertarBotao('win')
@@ -20,22 +20,28 @@ def pesquisar():
         sleep(2)
         c = c + 1
         
+def fecharEdge():
+    atalhoTeclado('ctrl', 'w')
+
 buscas = ["qual foi a primeira palavra registrada da historia",
     "crash bandicoot",
     "rio claro sp hoje",
     "michael jackson",
-    "o que aprendemos no curso de psicologia"
+    "gamepass hoje preco",
+    "o que aprendemos no curso de psicologia",
     "clima para essa semana",
     "aura farming desenvolvimento pessoal",
-    "porque e tao importante aprender a administrar uma empresa"
-    "porque eu não consigo ser amigo da minha ex-namorada"
-    "gears of war lançamento"
+    "porque e tao importante aprender a administrar uma empresa",
+    "porque eu nao consigo ser amigo da minha ex-namorada",
+    "gears of war lançamento",
     "noticias atuais sobre o brasil",
     "literatura para que serve e como a usamos sem perceber",
-    "app agendamento online gratuito"
-    "ideias de roupas para o aniversario da melhor amiga"
-    "o que raios e farmar aura e porque os jovens gostam tanto disso"
+    "app agendamento online gratuito",
+    "ideias de roupas para o aniversario da melhor amiga",
+    "o que raios e farmar aura e porque os jovens gostam tanto disso",
     "jogos de videogame 2026",
+    "internet fibra otica regiao de rio claro sp",
+    "porque aprender a administrar uma empresa",
     "minha namorada me pegou chorando no banho",
     "elementos literarios e para que servem",
     "para que escola se existe o google",
@@ -48,13 +54,16 @@ buscas = ["qual foi a primeira palavra registrada da historia",
     "origem da internet",
     "porque aprender defesa pessoal",
     "idade de cristiano ronaldo",
+    "aprender a administrar uma empresa",
     "jogos gratis de xbox",
     "app agendamento farmacia alto custo",
     "salario de advogado",
     "e se o messi fosse o melhor jogador de futebol do mundo",
     "como conseguir empregos usando Linkedin",
-    "qual e o metodo de ensino usado nas escolas e porque e tao ruim"
+    "qual e o metodo de ensino usado nas escolas e porque e tao ruim",
+    "github nao sincronizando com meu vs code"
 ]
 
 abrirEdge()
 pesquisar()
+fecharEdge()
